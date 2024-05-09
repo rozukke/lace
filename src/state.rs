2a2ab036b2fcca18eec591b5a833c64796e0858f
Artemis Rosman <artemis.rosman@gmail.com>
2024-05-10 02:13:55 +1000

    Add basic system state

---
+++ src/state.rs
@@ -0,0 +1,33 @@
+const MEMORY_MAX: u16 = xFFFF;
+
+// Represents complete program state
+// Of note - instructions are not stored in `mem`, but rather mapped to it.
+struct State {
+    // 128KB of system memory
+    mem: [u16; MEMORY_MAX],
+    // Address provided by program to indicate where to load user code
+    init_addr: u16,
+    // Program counter
+    pc: u16,
+    // 8x 16-bit registers 
+    reg: [u16; 8],
+    // Condition code
+    cc: Flag,
+    // Processor status register
+    psr: u16
+}
+
+// Set using result from previous instruction
+enum Flag {
+    // Negative
+    N,
+    // Zero
+    Z,
+    // Positive
+    P
+}
+
+struct SymbolTable {
+    // Hashmap of LABEL -> address
+    table: HashMap<String, u16>,
+}
