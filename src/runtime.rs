a9794f921cc6a3bc899fa41b397cad4237bcb122
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-08-10 01:02:01 +1000

    Update docs

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,6 +1,7 @@
 use crate::symbol::Flag;
 use std::collections::HashMap;
 
+/// LC3 can address 128KB of memory.
 const MEMORY_MAX: usize = 0x10000;
 
 /// Represents complete program state during runtime.
@@ -8,14 +9,14 @@ pub struct State {
     /// System memory - 128KB in size.
     /// Need to figure out if this would cause problems with the stack.
     mem: [u16; MEMORY_MAX],
-    // Address provided by program to indicate where to load user code
+    /// Address provided by program to indicate where to load user code
     init_addr: u16,
-    // Program counter
+    /// Program counter
     pc: u16,
-    // 8x 16-bit registers
+    /// 8x 16-bit registers
     reg: [u16; 8],
-    // Condition code
+    /// Condition code
     cc: Flag,
-    // Processor status register
+    /// Processor status register
     psr: u16,
 }
