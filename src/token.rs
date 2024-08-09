01464ee611db31468aab8b2c3a682c9c6530248a
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-08-09 23:58:03 +1000

    Remove old code

--- src/token.rs
+++
@@ -1,9 +0,0 @@
-/// Represents a single "word" inside the parsed representation of source code.
-pub struct Token {
-    // Value contained inside the token
-    pub val: String,
-    // Line number inside the file
-    pub line: u16,
-    // Column number inside line
-    pub col: u16,
-}
