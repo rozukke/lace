d61a9411fd33c5f43d4a30d702c764387973beff
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-09-20 22:24:30 +1000

    Add compile and run test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,6 @@
-use assert_cmd::prelude::*;
+use assert_cmd::Command;
 use predicates::str::contains;
-use std::process::Command;
+use tempfile::tempdir;
 
 #[test]
 fn runs_without_arguments() {
@@ -17,4 +17,23 @@ fn runs_hello_world() {
         .success()
         .stdout(contains("Hello, world!"))
         .stdout(contains("Halted"));
+}
+
+#[test]
+fn compile_and_run() {
+    let dir = tempdir().expect("Could not make tempdir");
+
+    let outfile_path = dir.path().join("hw.lc3");
+
+    let mut cmd = Command::cargo_bin("lace").unwrap();
+    cmd.arg("compile")
+        .arg("tests/files/hw.asm")
+        .arg(&outfile_path);
+
+    cmd.assert().success().stdout(contains("Saved target"));
+
+    let mut cmd = Command::cargo_bin("lace").unwrap();
+    cmd.arg(&outfile_path);
+
+    cmd.assert().success().stdout(contains("Hello, world!"));
 }
