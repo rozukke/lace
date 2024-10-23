847352c9f99933528171d1c66851839d208fa5e4
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-10-23 15:08:11 +1100

    Integration tests for stack instructions

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -20,6 +20,29 @@ fn runs_hello_world() {
 }
 
 #[test]
+fn runs_stack_example() {
+    let mut cmd = Command::cargo_bin("lace").unwrap();
+    cmd.arg("run").arg("tests/files/stack.asm");
+
+    cmd.assert()
+        .success()
+        .stdout(contains("Hello from the stack"))
+        .stdout(contains("Returned from call"))
+        .stdout(contains("R2 contents are 5"));
+}
+
+#[test]
+fn runs_recursive_fibonacci_example() {
+    let mut cmd = Command::cargo_bin("lace").unwrap();
+    cmd.arg("run").arg("tests/files/fibonacci.asm");
+
+    // Hardcoded 23rd fibonacci number
+    cmd.assert()
+        .success()
+        .stdout(contains("28657"));
+}
+
+#[test]
 fn compile_and_run() {
     let dir = tempdir().expect("Could not make tempdir");
 
