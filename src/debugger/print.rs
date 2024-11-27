dd16a6439e924a628448b3e08884adfd5de41327
darcy <acednes@gmail.com>
2024-11-27 21:19:02 +1100

    feat: strip colors if minimal

--- src/debugger/print.rs
+++ src/debugger/print.rs
@@ -5,9 +5,22 @@ use crate::runtime::terminal_cursor;
 pub const DEBUGGER_COLOR: u8 = 34;
 
 pub fn print(string: String) {
+    let minimal = false;
+
     let is_line_start = string.chars().next_back() == Some('\n');
 
-    eprint!("{}", ColoredString::from(string).blue());
+    if minimal {
+        let mut chars = string.chars();
+        while let Some(ch) = chars.next() {
+            if ch == '\x1b' {
+                while chars.next().is_some_and(|ch| ch != 'm') {}
+                continue;
+            }
+            eprint!("{}", ch);
+        }
+    } else {
+        eprint!("{}", ColoredString::from(string).blue());
+    }
 
     terminal_cursor::set_line_start(is_line_start);
 }
