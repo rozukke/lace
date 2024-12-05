525f1c842617880e6dd3d9faf8440d6f4e45d4a6
darcy <acednes@gmail.com>
2024-12-05 19:23:45 +1100

    refactor: use `StaticStr` struct to abstract unsafe behaviour

--- src/debugger/eval.rs
+++ src/debugger/eval.rs
@@ -6,17 +6,45 @@ use crate::{
     AsmParser,
 };
 
+/// Get an unsafe `&'static str` from a `Box<str>`, for temporary use
+struct StaticStr {
+    ptr: *mut str,
+}
+
+impl StaticStr {
+    pub unsafe fn as_str(&self) -> &'static str {
+        &*self.ptr
+    }
+}
+
+impl<T> From<T> for StaticStr
+where
+    T: Into<Box<str>>,
+{
+    fn from(string: T) -> Self {
+        Self {
+            ptr: Box::into_raw(string.into()),
+        }
+    }
+}
+
+impl Drop for StaticStr {
+    fn drop(&mut self) {
+        let boxed = unsafe { Box::from_raw(self.ptr) };
+        drop(boxed);
+    }
+}
+
 pub fn eval(state: &mut RunState, line: String) {
     // Required to make temporarily 'static
-    let line_ptr = Box::into_raw(line.into_boxed_str());
-    let line = unsafe { &*line_ptr };
+    // Automatically dropped at end of scope
+    let line = StaticStr::from(line);
 
-    // Do not return early, to ensure string is freed
-    if let Err(err) = run(state, line) {
+    // Note that error cannot be returned from this function, without the caller
+    // being responsible for dropping `line`
+    if let Err(err) = run(state, unsafe { line.as_str() }) {
         eprintln!("{:?}", err);
     }
-
-    unsafe { drop(Box::from_raw(line_ptr)) };
 }
 
 // TODO(refactor): Rename `run`
