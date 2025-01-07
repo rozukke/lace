c45df2e8e8f17fa465715bb645ff272cf515aa5c
darcy <acednes@gmail.com>
2025-01-07 11:23:09 +1100

    chore: restore `hw.asm`

--- tests/files/hw.asm
+++ tests/files/hw.asm
@@ -1,14 +1,6 @@
-    and r0, r0, #0
-
-Loop
-    REG
-
-    add r0, r0, #1
-    ld r1, NegMax
-    add r1, r0, r1
-    BRn Loop
-
-    HALT
-
-NegMax .FILL x-81
+; comment
+lea r0 hw
+puts
+halt
+hw .stringz "Hello, world!"
 
