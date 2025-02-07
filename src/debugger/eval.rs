5504a6e711bfe2948707f132698c996643739eb4
darcy <acednes@gmail.com>
2025-02-07 21:14:33 +1100

    refactor: remove unnecessary boxing to make `&str` static

--- src/debugger/eval.rs
+++ src/debugger/eval.rs
@@ -7,18 +7,14 @@ use crate::{dprintln, AsmParser};
 
 pub fn eval(state: &mut RunState, line: &str) {
     // Required to make temporarily 'static
-    // Automatically dropped at end of scope
-    let line = StaticStr::from(line);
-
-    // Note that error cannot be returned from this function, without the caller
-    // being responsible for dropping `line`
-    // TODO(doc): Write safety comment !!
-    if let Err(err) = eval_inner(state, unsafe { line.as_str() }) {
+    // SAFETY: `line` is not used after being dropped (i.e. not returned or used in a greater scope)
+    let line_static = unsafe { &*(line as *const str) };
+    if let Err(err) = eval_inner(state, line_static) {
         eprintln!("{:?}", err);
     }
 }
 
-// Wrapper to group errors into one location
+/// Wrapper to group errors into one location
 fn eval_inner(state: &mut RunState, line: &'static str) -> Result<()> {
     // Parse
     let stmt = AsmParser::new_simple(line)?.parse_simple()?;
@@ -45,33 +41,4 @@ fn eval_inner(state: &mut RunState, line: &'static str) -> Result<()> {
     state.execute(instr);
 
     Ok(())
-}
-
-/// Get an unsafe `&'static str` from a `Box<str>`, for temporary use.
-struct StaticStr {
-    ptr: *mut str,
-}
-
-impl StaticStr {
-    pub unsafe fn as_str(&self) -> &'static str {
-        &*self.ptr
-    }
-}
-
-impl<T> From<T> for StaticStr
-where
-    T: Into<Box<str>>,
-{
-    fn from(string: T) -> Self {
-        Self {
-            ptr: Box::into_raw(string.into()),
-        }
-    }
-}
-
-impl Drop for StaticStr {
-    fn drop(&mut self) {
-        let boxed = unsafe { Box::from_raw(self.ptr) };
-        drop(boxed);
-    }
 }
