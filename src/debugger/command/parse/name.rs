ad43cd41fe9bea69125823b6e69af6accff660ba
darcy <acednes@gmail.com>
2025-02-27 09:56:18 +1100

    refactor: use macro for command name list

--- src/debugger/command/parse/name.rs
+++ src/debugger/command/parse/name.rs
@@ -1,46 +1,90 @@
 use super::{error, Arguments, CommandName};
 
-#[rustfmt::skip]
-const COMMANDS: CommandNameList = &[
-    (CommandName::Help,        &["help", "--help", "h", "-h", "HELP", "man", "info", "wtf"], &[]),
-    (CommandName::Next,        &["step", "s"], &["next"]),
-    (CommandName::Step,        &["stepinto", "stepin", "step-into", "step-in", "stepi", "step-i", "si"], &[]),
-    (CommandName::Finish,      &["stepout", "step-out", "stepo", "step-o", "so"], &[]),
-    (CommandName::Continue,    &["continue", "cont", "con", "c"], &[]),
-    (CommandName::Get,         &["print", "p"], &[]),
-    (CommandName::Set,         &["move", "mov", "mv", "m"], &[]),
-    (CommandName::Registers,   &["registers", "register", "reg", "regs", "r"], &[]),
-    (CommandName::Jump,        &["goto", "go", "g"], &[]),
-    (CommandName::Source,      &["assembly", "asm", "a"], &[]),
-    (CommandName::Eval,        &["eval", "evil", "e"], &[]),
-    (CommandName::Reset,       &["reset"], &[]),
-    (CommandName::Echo,        &["echo"], &[]),
-    (CommandName::Quit,        &["quit", "q"], &[]),
-    (CommandName::Exit,        &["exit", "x", ":q", ":wq", "^C"], &[]),
-    (CommandName::BreakList,   &["breaklist", "bl"], &[]),
-    (CommandName::BreakAdd,    &["breakadd", "ba"], &[]),
-    (CommandName::BreakRemove, &["breakremove", "br"], &[]),
-    // "break" is treated specially
+macro_rules! name_list {
+    [
+        $(
+            $name:ident
+            [ $( $candidate:literal ),* $(,)? ]
+            [ $( $suggested:literal ),* $(,)? ]
+        )*
+    ] => {
+        &[
+            $( (
+                CommandName::$name,
+                &[ $($candidate,)* ],
+                &[ $($suggested,)* ],
+            ), )*
+        ]
+    };
+}
+const COMMANDS: CommandNameList = name_list![
+    Help
+        ["help", "--help", "h", "-h", "HELP", "man", "info", "wtf"]
+        []
+    Next
+        ["step", "s"]
+        ["next"]
+    Step
+        ["stepinto", "stepin", "step-into", "step-in", "stepi", "step-i", "si"]
+        []
+    Finish
+        ["stepout", "step-out", "stepo", "step-o", "so"]
+        ["finish", "fin"]
+    Continue
+        ["continue", "cont", "con", "c"]
+        []
+    Get
+        ["print", "p"]
+        ["get"]
+    Set
+        ["move", "mov", "mv", "m"]
+        ["set"]
+    Registers
+        ["registers", "register", "reg", "regs", "r"]
+        []
+    Jump
+        ["goto", "go", "g"]
+        ["jump", "jsr", "jsrr", "call"]
+    Source
+        ["assembly", "asm", "a"]
+        ["source", "src"]
+    Eval
+        ["eval", "evil", "e"]
+        ["run", "exec", "execute", "sim", "simulate"]
+    Reset
+        ["reset"]
+        []
+    Echo
+        ["echo"]
+        []
+    Quit
+        ["quit", "q"]
+        []
+    Exit
+        ["exit", "x", ":q", ":wq", "^C"]
+        ["halt", "end", "stop"]
+    BreakList
+        ["breaklist", "bl"]
+        []
+    BreakAdd
+        ["breakadd", "ba"]
+        []
+    BreakRemove
+        ["breakremove", "br"]
+        []
 ];
 const BREAK_COMMAND: CandidateList = &["break", "b"];
-#[rustfmt::skip]
-const BREAK_SUBCOMMANDS: CommandNameList = &[
-    (CommandName::BreakList,   &["list", "ls", "l"], &[]),
-    (CommandName::BreakAdd,    &["add", "a"], &[]),
-    (CommandName::BreakRemove, &["remove", "rm", "r"], &[]),
+const BREAK_SUBCOMMANDS: CommandNameList = name_list![
+    BreakList
+        ["list", "ls", "l"]
+        []
+    BreakAdd
+        ["add", "a"]
+        []
+    BreakRemove
+        ["remove", "rm", "r"]
+        []
 ];
-// #[rustfmt::skip]
-// const MISTAKE_COMMANDS: CommandNameList = &[
-//     (CommandName::Next,        &["next"]),
-//     (CommandName::Finish,      &["finish", "fin"]),
-//     (CommandName::Set,         &["set"]),
-//     (CommandName::Get,         &["get"]),
-//     (CommandName::Jump,        &["jump", "jmp", "jsr", "jsrr"]),
-//     (CommandName::Source,      &["source", "src"]),
-//     (CommandName::Eval,        &["run", "exec", "execute", "sim", "simulate"]),
-//     (CommandName::BreakList,   &["breakpoint", "breakp"]),
-//     (CommandName::Exit,        &["halt", "end", "stop"]),
-// ];
 
 impl Arguments<'_> {
     /// Parse next [`CommandName`].
