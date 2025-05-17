d8fd39af0d43c08e3c99b60d1809ffc9b3581679
nonl4331 <nicholasl0003@gmail.com>
2025-05-17 19:56:01 +1000

    Forgot to run RustFmt

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -191,7 +191,9 @@ impl Cursor<'_> {
     fn hex(&mut self) -> Result<TokenKind> {
         let start = self.abs_pos();
         let prefix = self.pos_in_token();
-        self.take_while(|c| c.is_numeric() || matches!(c.to_ascii_lowercase(), 'x' | '-' | 'a'..='f' ));
+        self.take_while(|c| {
+            c.is_numeric() || matches!(c.to_ascii_lowercase(), 'x' | '-' | 'a'..='f')
+        });
         let str_val = self.get_range(start..self.abs_pos());
         let value = match i16::from_str_radix(str_val, 16) {
             Ok(value) => value as u16,
